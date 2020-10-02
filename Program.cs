ef805bd8137582a197f2d05f7d2a3a45bf92f4e0
Dimitri <dimitri.defresne74@gmail.com>
2020-10-02 11:13:26 +0200

    lire fichier ok

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using System.IO;
 using System;
 
 namespace ClubFromage
@@ -10,9 +11,30 @@ namespace ClubFromage
             //Console.WriteLine("Bonjour !");
             //DBAL db = new DBAL();
             //db.Insert(Console.ReadLine());
-            
 
             
-        }
+                int compteur = 0;
+                string ligne;
+                Char caractere = ';';
+
+                StreamReader fichier = new StreamReader(@"D:\Dimitri DEFRESNE\fichier csv\Pays.csv");
+
+                while ((ligne = fichier.ReadLine()) != null)
+                {
+                    String[] substrings = ligne.Split(caractere);
+
+                    foreach (var substring in substrings)
+                    {
+                        Console.WriteLine(substring);
+                    }
+
+                    compteur++;
+                }
+
+                fichier.Close();
+                System.Console.WriteLine("Nombre de lignes : {0}.", compteur);
+            }
+
+        
     }
 }
--- pays.cs
+++ pays.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 
-public class Class1
+public class Pays
 {
-	public Class1()
+	private int _id;
+	private string _nom;
+	public Pays(int UnID,string NomPays)
 	{
+		_id = UnID;
+		_nom = NomPays;
+
+
 	}
+
+    public int Id { get => _id; set => _id = value; }
+    public string Nom { get => _nom; set => _nom = value; }
 }
