0178777ffa8207ba54ad51a7a92461b2d3ac15e9
Dimitri <dimitri.defresne74@gmail.com>
2020-10-05 15:20:12 +0200

    class_fromageetpays

--- Pays.cs
+++ Pays.cs
@@ -2,9 +2,14 @@
 
 public class Pays
 {
-	private int _id;
-	private string _nom;
-	public Pays()
+    private int _id;
+    private string _nom;
+	public Pays(int UnID,string UnNom)
 	{
+		_id = UnID;
+		_nom = UnNom;
 	}
+
+    public string Nom { get => _nom; set => _nom = value; }
+    protected int Id { get => _id; set => _id = value; }
 }
